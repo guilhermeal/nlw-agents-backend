df5028de18aedcd8ec48a3388dafacc1c5191e0f
Guilherme Almeida <guiallan.al@gmail.com>
2025-07-08 23:43:59 -0300

    feat: initialize server with Fastify and TypeScript
    - Add package.json with dependencies and scripts for starting the server
    - Create env.ts for environment variable validation using Zod
    - Implement server.ts with Fastify setup, including CORS and health check endpoint
    - Add tsconfig.json for TypeScript configuration

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { fastifyCors } from "@fastify/cors";
+import { env } from "./env.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5173",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.get("/health", () => {
+  return "OK";
+});
+
+app.listen({ port: env.PORT }).then(() => {
+  // Remover este console futuramente
+  console.log(`\n🚀 Server is running on http://${env.HOST}:${env.PORT} 💥\n`);
+});
